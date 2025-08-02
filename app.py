88ff685ae3ffa95480b509587d3766cf2c529ea9
coding-enthusiast003 <subhamdas1029@gmail.com>
2025-08-03 00:41:21 +0530

    connected to momgodb atlas

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['MAIL_DEFAULT_SENDER'] = 'rishi03puri@gmail.com'
 
 mail = Mail(app)
 
-app.config["MONGO_URI"] = "mongodb+srv://subham:<db_password>@cluster0.zl23awz.mongodb.net/newsdb?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://subham:RgZ6FaijzSfc9Pzb@cluster0.kcpfkoi.mongodb.net/db1?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 mongo.db.newsdb.create_index("fetched_at", expireAfterSeconds = 129600) # cached data will expire after 36 hours
