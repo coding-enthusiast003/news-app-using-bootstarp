2231fdbb1460bee50676e13aae32cb9a81ffdcff
coding-enthusiast003 <subhamdas1029@gmail.com>
2025-08-03 21:06:17 +0530

    Add python-dotenv to requirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, render_template
 import requests
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
